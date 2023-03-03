f00211ca413b49fda4a19840272ae333bd234234
ilechir <108686043+ilechir@users.noreply.github.com>
2023-03-03 02:01:07 -0300

    re-haciendo el back parte 1

---
+++ src/main/java/com/myPortfolio/IC/Controller/CPersona.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myPortfolio.IC.Controller;
+
+import com.myPortfolio.IC.Dto.dtoPersona;
+import com.myPortfolio.IC.Entity.Persona;
+import com.myPortfolio.IC.Security.Controller.Mensaje;
+import com.myPortfolio.IC.Service.ImpPersonaService;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author arace
+ */
+@RestController
+@RequestMapping("/personas")
+@CrossOrigin(origins = "http://localhost:4200")
+        
+public class CPersona {
+  @Autowired 
+    ImpPersonaService personaS;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Persona>> list(){
+        List<Persona> list = personaS.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+      @GetMapping("/detail/{id}")
+    public ResponseEntity<Persona> getById(@PathVariable("id")int id){
+        if(!personaS.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = personaS.getOne(id).get();
+        return new ResponseEntity(persona, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtopersona){
+        if(!personaS.existsById(id)){
+            return new ResponseEntity(new Mensaje("No exites el id"), HttpStatus.NOT_FOUND);
+        }
+       if(personaS.existsByNombre(dtopersona.getNombre()) && personaS.getByNombre(dtopersona.getNombre()).get().getId() !=id){
+           return new ResponseEntity(new Mensaje("Ese nombre ya exite"), HttpStatus.BAD_REQUEST);
+       }
+          if(StringUtils.isBlank(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
+        }
+          if(StringUtils.isBlank(dtopersona.getDescripcion())){
+            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = personaS.getOne(id).get();
+        
+        persona.setNombre(dtopersona.getNombre());
+        persona.setDescripcion(dtopersona.getDescripcion());
+        persona.setApellido(dtopersona.getApellido());
+        persona.setImg(dtopersona.getImg());
+        
+        personaS.save(persona);
+        
+        return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);   
+       
+    }
+}
---
+++ src/main/java/com/myPortfolio/IC/Repository/RPersona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myPortfolio.IC.Repository;
+
+import com.myPortfolio.IC.Entity.Persona;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author arace
+ */
+@Repository
+public interface RPersona extends JpaRepository<Persona, Integer> {
+    
+    
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+    
+}
